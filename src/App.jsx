1e83d4925b27ee795a9200ab98488a41e954d2dd
Sahil Kharatmol <sahilkharatmol09@gmail.com>
2024-01-09 14:59:58 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import './App.css';
-import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
 
-function App() {
+
+function App(imgData) {
   // code here
+  
+  const images = imgData.imgData;
+  
+  return(
+    <div>
+  
+      <h1>Kalvium Gallery</h1>
+
+      <div id='gir'> 
+          {images.map((element) => (
+          <img src={element.img} id={element.id}/>
+        
+          ))};
+      </div>
+   
+    </div>
+ )
+
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -4,27 +4,26 @@ import elephant from "./images/elephant.jpeg";
 
 export default class AppClass extends Component{
   
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
+  
+
+  render(){
+    const images = this.props.imgData;
+
+    return(
+      <div>
+    
+        <h1>Kalvium Gallery</h1>
+
+        <div id='gir'> 
+            {images.map((element) => (
+            <img src={element.img} id={element.id}/>
+          
+            ))};
+        </div>
+     
+      </div>
+   )
   }
 
-  // code here
+
 }
