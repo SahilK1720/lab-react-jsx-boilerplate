80f2a81f795754f3a5b5468df0a452727586de92
Sahil Kharatmol <144338853+SahilK1720@users.noreply.github.com>
2024-01-11 11:46:56 +0530

    done

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -4,10 +4,30 @@ import elephant from "./images/elephant.jpeg";
 
 export default class AppClass extends Component{
   
-  
+  imageData = ()=>{
+    let data = [
+      {
+        id:1,
+        img:elephant
+      },
+      {
+        id:2,
+        img:elephant
+      },
+      {
+        id:3,
+        img:elephant
+      },
+      {
+        id:4,
+        img:elephant
+      }
+    ]
+    return data;
+  }
 
   render(){
-    const images = this.props.imgData;
+    const images = this.imageData();
 
     return(
       <div>
